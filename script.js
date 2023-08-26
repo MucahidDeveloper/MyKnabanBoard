9dd15f2d4192001c99dfc4577d42ed5bf9962faf
MucahidDeveloper <Mucadad221@gmail.com>
2023-08-26 15:01:14 +0300

    finish styling finally!

---
+++ script.js
@@ -0,0 +1,14 @@
+const relaxer = document.querySelector(".relaxer");
+
+// relaxer.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   fetch("https://qurancentral.com/mishary-rashid-alafasy-001-al-fatiha/").then(
+//     (res) => {
+//       console.log("fetch succed");
+//       console.log(res);
+//       console.log(res.json());
+//       console.log(res.json(result).parse());
+//       // res.json();
+//     }
+//   );
+// });
